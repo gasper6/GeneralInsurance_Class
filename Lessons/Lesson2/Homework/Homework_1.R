4e1748505a54472c799589b73882dd722e540edc
gasper6 <gasper6@uniba.sk>
2019-03-04 23:04:23 +0100

    Finished homework. I Still have no idea what was I supposed to do.

--- Lessons/Lesson2/Homework/Homework_1.R
+++ Lessons/Lesson2/Homework/Homework_1.R
@@ -13,36 +13,31 @@ dt_KPI <- read.csv("./Data/lesson2_KPI.csv")
 dt_KPI$Premium[dt_KPI$Premium < 0] = 0
 # Nemám rád dplyr
 
+dt_KPI %>%
+  mutate(Profit = Premium - Expenses - Losses) %>% 
+  group_by(Unit) %>% 
+  summarize(Profit = sum(Profit, na.rm = TRUE)) %>% 
+  arrange(desc(Profit))
+# Najlepšia unit bola 7
 
+dt_KPI %>% filter(Unit == "Unit7") %>%
+  mutate(Profit = Premium - Expenses - Losses) %>% 
+  group_by(Year) %>% 
+  summarize(Profit = sum(Profit, na.rm = TRUE)) %>% 
+  arrange(Profit)
+# najhorší rok bol 2014
 
-
-
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-
-
-
+dt_KPI %>% filter(Unit == "Unit7") %>%
+  mutate(Profit = Premium - Expenses - Losses) %>% 
+  group_by(Year) %>% 
+  summarize(Profit = sum(Profit, na.rm = TRUE)) %>% 
+  ggplot(aes(x = Year, y = Profit)) + 
+  geom_col()
+# Pekne nakreslíme
 
 # Your Explanation about analysis:
-# 
-# 
-# 
+
+# Netuším ako súvisela príprava dát s týmto cvičením, ani ako sa mala správne urobiť. Blbé dáta sa mali vynechať? Mali sa nhradiť priemerom? Čo tam bolo treba urobiť?
+# Záporné premium som nahradil nulami. Pekne po starom, ako R-ku
+# Potom som zisťoval ktorá unit mala najväčší zisk. Zistil som, že to bola Unit7
+# Dal som si vykresliť profit Unit7 podľa rokov. Aj na grafe vidno, že najhorší rok bol 2014.
